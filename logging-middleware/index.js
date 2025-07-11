86e0368ebf48e941cba4e512c3a0a1dfaeb7a2f7
Abhay Rajput <abhayravirajput@gmail.com>
2025-07-11 14:18:56 +0530

    added URL shortner API

--- logging-middleware/index.js
+++ logging-middleware/index.js
@@ -1,16 +1,26 @@
+// index.js
 const axios = require("axios");
 
+/**
+ * Log function to send logs to the test server
+ * 
+ * @param {string} stack - "backend" or "frontend"
+ * @param {string} level - log level: "info", "warn", "error", "fatal"
+ * @param {string} pkg - the part of app like "shortener", "redirect", "validation"
+ * @param {string} message - what happened
+ */
 async function Log(stack, level, pkg, message) {
   try {
-    const res = await axios.post("http://20.244.56.144/logging-service/logs", {
-      stack,
-      level,
+    const response = await axios.post("http://20.244.56.144/logging-service/logs", {
+      stack: stack,
+      level: level,
       package: pkg,
-      message
+      message: message
     });
-    console.log("Log Success:", res.data.message);
-  } catch (err) {
-    console.error("Log Error:", err.message);
+
+    console.log("Log Success:", response.data.message);
+  } catch (error) {
+    console.error("Log Failed:", error.message);
   }
 }
 
